8aa5f3a99679beca58351814ac7a2c26d1d62594
Nataliia Kozyriatska <kozyriatskanataliia@gmail.com>
2025-03-01 17:58:46 +0200

    Solution

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@
 import './App.scss';
 // import moviesFromServer from './api/movies.json';
 import movies from './api/movies.json';
-import MovieList from './components/MovieList';
+import { MovieList } from './components/MovieList';
 
 export const App = () => (
   <div className="page">
